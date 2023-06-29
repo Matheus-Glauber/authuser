046f574d142d2b74c14b0278c9742ba9b342d053
Matheus Glauber <glauber.jordao1995@gmail.com>
2023-06-29 14:09:47 -0300

    adicionando interfaces para diferentes dtos

--- src/main/java/com/ead/authuser/dtos/UserDto.java
+++ src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -1,17 +1,34 @@
 package com.ead.authuser.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 
 import java.util.UUID;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserDto(
         UUID userId,
+        @JsonView(UserView.RegistrationPost.class)
         String username,
+        @JsonView(UserView.RegistrationPost.class)
         String email,
+        @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
         String password,
+        @JsonView(UserView.PasswordPut.class)
         String oldPassword,
+        @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
         String fullName,
+        @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
         String phoneNumber,
+        @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
         String cpf,
-        String imageUrl) {}
+        @JsonView({UserView.RegistrationPost.class, UserView.ImagePut.class})
+        String imageUrl) {
+
+    public interface UserView {
+        interface RegistrationPost{}
+        interface UserPut{}
+        interface PasswordPut{}
+        interface ImagePut{}
+    }
+}
